4291838cad96e7c2011c326892c18765221cad51
Daniel Andersson <d.andersson86@icloud.com>
2021-09-10 09:40:15 +0200

    Adding NPM script for direct build & deployment. Adding a public path for application in production.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
+/**
+ * Module Exports.
+ * @type {{presets: string[], publicPath: string}}
+ */
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  publicPath: "./"
 }
