671974f27b3a7d07f77229e940b2f276a0fa7255
Daniel Andersson <d.andersson86@icloud.com>
2021-09-28 19:01:03 +0200

    Added dependencies for testing, Jest & Cypress. Updated vue dependency version in package.json.

---
+++ tests/unit/tinyMCE.spec.js
@@ -0,0 +1,18 @@
+// /**
+//  * Import Module Dependencies
+//  */
+// import { mount } from 'vue-test-utils';
+// import theEditor from "@/components/TextEditor/TinyMceEditor";
+//
+//
+//
+// /**
+//  * Test Suite.
+//  */
+// describe("theEditor.test.js", () => {
+//     let wrapper = mount(theEditor);
+//
+//     it('Something something in here when you figure it out...', () => {
+//
+//     });
+// });
